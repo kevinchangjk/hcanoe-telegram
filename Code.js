2e2911eca9f60682ad3d80c83fbff45e6beab255
nguyenvukhang <brew4k@gmail.com>
2021-11-25 11:03:10 +0800

    sheets + drive file movements up and running

--- Code.js
+++ Code.js
@@ -1,7 +1,7 @@
 var token = '2113008414:AAH4CbDxNzHnA28I2yS-3uJHyW8LQXTBN-U'
 var telegramAppUrl = 'https://api.telegram.org/bot' + token
 var webAppUrl =
-  'https://script.google.com/macros/s/AKfycbwIKAWH2iqUMG6JA9xVUXuwt5lKtlrBwWTSi8GTiIE8rhz0nevGdA6qZPqLBJZBogoF/exec'
+  'https://script.google.com/macros/s/AKfycbzcFpg2hNfBR7JiZTpqn-9etmDfiE8v-tOFOcTVDRfdfs6xxjC6huhPopssEHDkGzxo/exec'
 
 function setWebhook() {
   var url = telegramAppUrl + '/setWebhook?url=' + webAppUrl
@@ -19,11 +19,45 @@ function doPost(e) {
   var contents = JSON.parse(e.postData.contents)
   var id = contents.message.from.id
   var text = contents.message.text
+  var teleUser = contents.message.from.username
 
-  sendMessage(id, 'deployment 8')
+  sendMessage(id, 'deployment 12')
   sendMessage(id, text)
 
   var databaseSheetId = '1FOi7blMjvtZeM7hqWYN_pTp9wiHDr-RzZG8pD3aBgV8'
   var sheet = SpreadsheetApp.openById(databaseSheetId).getSheets()[0]
-  sheet.appendRow([text, "second column"])
+
+  var tempSheetTitle = teleUser + "-hcanoe-temp-input"
+
+  var tempSheet = SpreadsheetApp.create(tempSheetTitle)
+  var tempSheetUrl = tempSheet.getUrl()
+  var tempSheetId = tempSheet.getId()
+  tempSheet.addEditor("nikelyvengun@gmail.com")
+
+  sendMessage(id, "your temporary input sheet url is " + tempSheetUrl)
+  sheet.appendRow([timestamp(), text, "second column"])
+
+  /* move the file, specifically to brew4k@gmail.com's
+   * `/My Drive/hcanoe/temp` directory
+   */
+  var source = DriveApp.getFileById(tempSheetId)
+  var target = DriveApp.getFolderById("1iQDiZbWZkro--EQy-CyA0Y5SnSIplPkd")
+  source.moveTo(target)
+}
+
+/* deployment check */
+// function doPost(e) {
+//   var contents = JSON.parse(e.postData.contents)
+//   var id = contents.message.from.id
+//   var text = contents.message.text
+//   var teleUser = contents.message.from.username
+// 
+//   sendMessage(id, 'deployment 12')
+//   sendMessage(id, text)
+// }
+
+function timestamp() {
+  var ts = new Date
+  Logger.log(ts)
+  return ts
 }
--- pu.sh
+++ pu.sh
@@ -2,5 +2,5 @@
 
 clasp push
 clasp deploy \
-  -d "hcanoe-telegram" \
-  -i "AKfycbwIKAWH2iqUMG6JA9xVUXuwt5lKtlrBwWTSi8GTiIE8rhz0nevGdA6qZPqLBJZBogoF"
+  -d 'hcanoe-telegram' \
+  -i 'AKfycbzcFpg2hNfBR7JiZTpqn-9etmDfiE8v-tOFOcTVDRfdfs6xxjC6huhPopssEHDkGzxo'
