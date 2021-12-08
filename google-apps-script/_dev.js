cd2ef7fcc9cfe329ba5a7e2427a5e341ba7a7a8d
nguyenvukhang <brew4k@gmail.com>
2021-12-08 23:04:27 +0800

    fixed minor bugs

---
+++ google-apps-script/_dev.js
@@ -0,0 +1,10 @@
+const deploymentMessage = "[dev] 8 Dec, 23:03:47";
+const token = "2113008414:AAH4CbDxNzHnA28I2yS-3uJHyW8LQXTBN-U";
+const deploymentId =
+  "AKfycbzGPrT3kZ8h7MiDPE5os1e7Mo-r_Fb_oIR-6Fqq05Mjwjk-yueUogpFZ8R4wc6GnzQh";
+
+function console() {
+  bot.whoami(257481278, tempFolderId, userDB);
+  bot.create(257481278, tempFolderId);
+  Logger.log("hello there");
+}
