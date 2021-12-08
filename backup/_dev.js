4ccbbcaf97fd0987533709b60a08fdf65c2b282c
nguyenvukhang <brew4k@gmail.com>
2021-12-08 21:35:29 +0800

    wrote a branch-based deployment script

---
+++ backup/_dev.js
@@ -0,0 +1,10 @@
+const deploymentMessage = "[]";
+const token = "";
+const deploymentId =
+  "AKfycbzGPrT3kZ8h7MiDPE5os1e7Mo-r_Fb_oIR-6Fqq05Mjwjk-yueUogpFZ8R4wc6GnzQh";
+
+function console() {
+  bot.whoami(257481278, tempFolderId, userDB);
+  bot.create(257481278, tempFolderId);
+  Logger.log("hello there");
+}
